5aca7201af785979d6e77e6fe0f58b74f82cd4c8
Zak Bainazarov <zakbain@gmail.com>
2018-03-27 01:52:32 -0400

    Added methods

--- src/com/zak/ai/Cell.java
+++ src/com/zak/ai/Cell.java
@@ -1,7 +1,14 @@
 package com.zak.ai;
 
+/**
+ * A single cell in the game of life.
+ * 
+ * @author Zak Bainazarov
+ *
+ */
 public class Cell {
 	private int liveNeighbors;
 	private int totalNeighbors;
 	private boolean alive;
+	private boolean nextState;
 }
--- src/com/zak/ai/CellGrid.java
+++ src/com/zak/ai/CellGrid.java
@@ -1,5 +1,19 @@
 package com.zak.ai;
 
+/**
+ * Cell Grid model and necessary calculations.
+ * 
+ * @author Zak Bainazarov
+ *
+ */
 public class CellGrid {
-
+	// Evaluate and store the next state
+	public void evaluateNextState() {
+		
+	}
+	
+	// Evaluate the number of neighbors for each cell
+	private void countNeighbors() {
+		
+	}
 }
